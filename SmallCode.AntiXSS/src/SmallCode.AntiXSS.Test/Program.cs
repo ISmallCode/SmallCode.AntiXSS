543cba4a4abae0f14d7dcde098336d8a43531f56
applenele <1941973283@qq.com>
2016-07-01 23:39:32 +0800

    rtm

--- SmallCode.AntiXSS/src/SmallCode.AntiXSS.Test/Program.cs
+++ SmallCode.AntiXSS/src/SmallCode.AntiXSS.Test/Program.cs
@@ -12,11 +12,11 @@ namespace SmallCode.AntiXSS.Test
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+                  .UseKestrel()
+                  .UseContentRoot(Directory.GetCurrentDirectory())
+                  .UseIISIntegration()
+                  .UseStartup<Startup>()
+                  .Build();
 
             host.Run();
         }
